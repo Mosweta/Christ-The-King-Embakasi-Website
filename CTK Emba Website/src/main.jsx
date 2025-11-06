7364a3831fbf861147c508b762660191381ab41d
mosweta <moswetaDeogracious365@gmail.com>
2025-11-06 11:55:48 +0300

    feat: add route to serve contact form and update contact form with reCAPTCHA site key added a route to direct to contact form

--- CTK Emba Website/src/main.jsx
+++ CTK Emba Website/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './ComingSoon.css'
 import ComingSoon from './ComingSoon.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContactForm from './Contactform.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<ComingSoon />} />
+      <Route path="/contactus" element={<ContactForm />} />
       
     </Routes>
     </BrowserRouter>
